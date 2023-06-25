2216f4568895d84ac4d65a801a8e526716e057e4
Brenno Faro <94204696+brenofaro@users.noreply.github.com>
2023-06-25 12:36:37 -0300

    Aula 192 - Melhoria da resolução do exercício

--- aula192.py
+++ aula192.py
@@ -7,11 +7,12 @@ lista_excluidos = []
 
 
 def ler_lista(lista):
-    if lista:
-        for i, valor in enumerate(lista):
-            print(f"Valor {i} = {valor}")
-    else:
+    if not lista:
         print("A lista atual está vazia!")
+        return
+
+    for i, valor in enumerate(lista):
+        print(f"Valor {i} = {valor}")
 
 
 def adicionar_tarefa(lista, tarefa):
@@ -19,17 +20,17 @@ def adicionar_tarefa(lista, tarefa):
 
 
 def desfazer_acao(lista, lista_excluidos):
-    if lista:
-        lista_excluidos.append(lista.pop())
-    else:
+    if not lista:
         print("Você não tem ações pra desfazer!")
+        return
+    lista_excluidos.append(lista.pop())
 
 
 def refazer_acao(lista, lista_excluidos):
-    if lista_excluidos:
-        lista.append(lista_excluidos.pop())
-    else:
+    if not lista_excluidos:
         print("Você não tem acoes para refazer!")
+        return
+    lista.append(lista_excluidos.pop())
 
 
 while True:
@@ -37,18 +38,31 @@ while True:
     print("Comandos: listar, desfazer, refazer")
     entrada = input("Digite uma tarefa ou comando: ")
 
-    if entrada == 'desfazer':
-        desfazer_acao(lista_tarefas, lista_excluidos)
-        continue
-    elif entrada == 'refazer':
-        refazer_acao(lista_tarefas, lista_excluidos)
-        continue
-    elif entrada == 'listar':
-        ler_lista(lista_tarefas)
-        continue
-    elif entrada == 'sair':
-        print("Saindo do programa")
-        break
-    else:
-        adicionar_tarefa(lista_tarefas, entrada)
-        continue
+    # Utilizando lambda para adiar a execução da função
+    comandos = {
+        'listar': lambda: ler_lista(lista_tarefas),
+        'refazer': lambda: refazer_acao(lista_tarefas, lista_excluidos),
+        'desfazer': lambda: desfazer_acao(lista_tarefas, lista_excluidos),
+        'adicionar': lambda: adicionar_tarefa(lista_tarefas, entrada)
+    }
+
+    # Adiando a execucao da funcao
+    comando = comandos.get(entrada) if comandos.get(entrada) is not None else comandos['adicionar']
+    # Executando agora a funcao
+    comando()
+
+    # if entrada == 'desfazer':
+    #     desfazer_acao(lista_tarefas, lista_excluidos)
+    #     continue
+    # elif entrada == 'refazer':
+    #     refazer_acao(lista_tarefas, lista_excluidos)
+    #     continue
+    # elif entrada == 'listar':
+    #     ler_lista(lista_tarefas)
+    #     continue
+    # elif entrada == 'sair':
+    #     print("Saindo do programa")
+    #     break
+    # else:
+    #     adicionar_tarefa(lista_tarefas, entrada)
+    #     continue
