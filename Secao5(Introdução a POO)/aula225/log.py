6ca129a1697d6f5ad4cd6e887514e19319eb3828
Brenno Faro <94204696+brenofaro@users.noreply.github.com>
2023-07-02 17:50:51 -0300

    Aula 226 - (Parte 2) Log, LogPrintMixin e a união de tudo até aqui

--- Secao5(Introdução a POO)/aula225/log.py
+++ Secao5(Introdução a POO)/aula225/log.py
@@ -1,16 +1,27 @@
 # Abstração
 
 class Log:
-    def log(self, msg):
+    def _log(self, msg):
         raise NotImplementedError("Implemente o método log")
+
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
 
 
 class LogFileMixin(Log):
-    def log(self, msg):
+    def _log(self, msg):
         print(msg)
 
 
-if __name__ == '__main__':
-    l = LogFileMixin()
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
 
-    l.log("Qualquer")
+
+if __name__ == '__main__':
+    l = LogPrintMixin()
+    l._log("Qualquer")
+    l.log_error("Envio")
